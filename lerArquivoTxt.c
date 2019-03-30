285716e7495ed4ebdd70bfb12de2ee737b3352d6
Dado CWB <44261704+DadoCWB@users.noreply.github.com>
2019-03-30 08:58:30 -0300

    Update lerArquivoTxt.c

--- lerArquivoTxt.c
+++ lerArquivoTxt.c
@@ -1,13 +1,13 @@
 
 /*	=====================================================================================================================================================
-	DescriÁ„o:	Esse cÛdigo tem por objetivo 	1) abrir um arquivo .txt;
-												2) Ler as 3 primeiras linhas desse arquivo;
-												3) armazenar cada linha em uma string;
-												4) imprimir  conteudo da string na tela
+	Descri√ß√£o:	Esse c√≥digo tem por objetivo 	1) abrir um arquivo .txt;
+							2) Ler as 3 primeiras linhas desse arquivo;
+							3) armazenar cada linha em uma string;
+							4) imprimir  conteudo da string na tela
 	-----------------------------------------------------------------------------------------------------------------------------------------------------
-	Vers„o 1.0
-	˙ltima atualizaÁ„o: 30/03/2019
-	disponÌvel em: https://github.com/DadoCWB/Estrutura_de_Dados
+	Vers√£o 1.0
+	√∫ltima atualiza√ß√£o: 30/03/2019
+	dispon√≠vel em: https://github.com/DadoCWB/Estrutura_de_Dados/edit/master/lerArquivoTxt.c
 	=====================================================================================================================================================
 */
 
@@ -16,17 +16,17 @@
 
 int main(){
 	
-	int i;			// contador 
-	char txt[50];	//string para armazera os caracteres de cada linha lida dento do laÁo pelo fscanf  
-					//estou supondo que uma linha n„o ir· conter mais que 50 caracteres
+	int i;	// vari√°vel auxilias (contador) 
+	char txt[50];	//string para armazera os caracteres de cada linha lida dento do la√ßo pelo fscanf  
+			//estou supondo que uma linha n√£o ir√° conter mais que 50 caracteres
 	
 	FILE *arq; //Ponteiro para abrir o arquivo txt
 	
 	arq=fopen("NomeRG10.txt", "r"); //nome do arquivo a ser aberto
-									// o "r" significa que o arquivo ser· aberto em modo leitura 
+					// o "r" significa que o arquivo ser√° aberto em modo leitura 
 	
 	for(i=0;i<3;i++){
-		fscanf(arq,"%s",txt); // lÍ uma linha do arquivo e armazena-a na string txt
+		fscanf(arq,"%s",txt); // l√™ uma linha do arquivo e armazena-a na string txt
 		
 		printf("\n%s",txt); //imprime na tela cada uma das linhas que foi lida pelo fscanf
 	}
@@ -36,15 +36,15 @@ int main(){
 /*
 Explicando um pouco as coisas:
 	
-	O ponteiro na linha 23 È do tipo FILE. Ele È usado para varrer cada caractere contido no arquivo e
-	armazen·-los na string txt.
+	O ponteiro na linha 23 √© do tipo FILE. Ele √© usado para varrer cada caractere contido no arquivo e
+	armazen√°-los na string txt.
  
-	fscanf È bem parecido com scanf . A diferenÁa È que scanf lÍ o teclado e fscanf lÍ um arquivo.
+	fscanf √© bem parecido com scanf . A diferen√ßa √© que scanf l√™ o teclado e fscanf l√™ um arquivo.
 	
-	para ler TODAS as linhas do arquivo ao invÈs de 3 recomenda-se:
+	para ler TODAS as linhas do arquivo ao inv√©s de 3 recomenda-se:
 	
-	while(!feof(arq)){ // le o arquivo atÈ a ultima linha
-  		fscanf(arq,"%s",txt); // lÍ uma linha do arquivo e armazena-a na string txt
+	while(!feof(arq)){ // le o arquivo at√© a ultima linha
+  		fscanf(arq,"%s",txt); // l√™ uma linha do arquivo e armazena-a na string txt
 		
 		printf("\n%s",txt); //imprime na tela cada uma das linhas que foi lida pelo fscanf 
 	}
